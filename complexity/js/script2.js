fc57195ece57f840873ef1b50731640a7352c174
thomas-leduc <thomas.leduc@crenau.archi.fr>
2021-07-22 15:14:09 +0200

    Update js/script2.js

--- complexity/js/script2.js
+++ complexity/js/script2.js
@@ -1,15 +1,13 @@
-function listClusterImages( complexities, complexityLevel ) {
+function listClusterImages(cookies, complexityLevel) {
     tmp = "";
-    for (var i = 0; i < complexities[complexityLevel].length; i++) {
-	cityId = complexities[complexityLevel][i];
+    for (var i = 0; i < cookies[complexityLevel].length; i++) {
+	cityId = cookies[complexityLevel][i];
 	tmp += `  <li class="ui-state-default">${cityId}. ${cities[cityId]} <img src="thumbnails/${cityId}a.jpg" width="300px" value="${cityId}" /></li>\n`;
     }
     return tmp;
 }
 
 function submit2() {
-    var cookies = parseCookies();
-    var date2 = new Date(Date.now());
     var complexities = { 'low': [], 'medium': [], 'high': [] };
     $("#sortable1").find("img").filter(function() {
 	complexities.low.push( Number($(this).attr("value")) );
@@ -21,46 +19,48 @@ function submit2() {
 	complexities.high.push( Number($(this).attr("value")) );
     });
 
+    document.cookie = `date2=${new Date(Date.now())}; SameSite=Strict`;
+    document.cookie = `lowComplexity=${complexities.low}; SameSite=Strict`;
+    document.cookie = `mediumComplexity=${complexities.medium}; SameSite=Strict`;
+    document.cookie = `highComplexity=${complexities.high}; SameSite=Strict`;
+
     //~ $("#sortable1").find("img").filter(function() { console.log('===', $(this).attr("value")); });
-    
-    console.log("***", complexities.low); //~ DEBUG
-    console.log("***", complexities.medium); //~ DEBUG
-    console.log("***", complexities.high); //~ DEBUG
-    
-    // DELETE COOKIES
-    document.cookie = "uid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "fname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "lname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "city=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "gender=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "age=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "skills=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "date0=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "date1=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
+    console.log("***", document.cookie); //~ DEBUG
 
-    document.cookie = "lowComplexity=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "mediumComplexity=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
-    document.cookie = "highComplexity=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
+    var cookies = parseCookies();
+    var finalResult = surveyContent(cookies);
     
-    finalResult = `
-uid = ${cookies.uid}
-fname = ${cookies.fname}
-lname = ${cookies.lname}
-city = ${cookies.city}
-gender = ${cookies.gender}
-age = ${cookies.age}
-skills = ${cookies.skills}
-date0 = ${cookies.date0}
-date1 = ${cookies.date1}
-date2 = ${date2}
-low = ${complexities.low}
-medium = ${complexities.medium}
-high = ${complexities.high}
-`;
-
-    //~ console.log("***", document.cookie); //~ DEBUG
-    alert(finalResult);
-    //~ window.location.href = "index53.html";
+    if ('Blob' in window) {
+	//~ var fileName = prompt('Please enter file name to save', 'Untitled.txt');
+	var fileName = `complexity-${cookies.uid}.txt`;
+	var textFileAsBlob = new Blob([finalResult], { type: 'text/plain' });
+	if ('msSaveOrOpenBlob' in navigator) {
+	    navigator.msSaveOrOpenBlob(textFileAsBlob, fileName);
+	} else {
+            var downloadLink = document.createElement('a');
+            downloadLink.download = fileName;
+            downloadLink.innerHTML = 'Download File';
+            
+            if ('webkitURL' in window) {
+		// Chrome allows the link to be clicked without actually adding it to the DOM.
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+            } else {
+		// Firefox requires the link to be added to the DOM before it can be clicked.
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.click(function(){
+		    document.body.removeChild(event.target);
+		}); 
+		
+		downloadLink.style.display = 'none';
+		document.body.appendChild(downloadLink);
+            }
+            downloadLink.click();
+	}
+    } else {
+	alert('Your browser does not support the HTML5 Blob.');
+    }
+    //~ alert(finalResult); //~ DEBUG
+    window.location.href = "index53.html";
 }
 
 $(document).ready(function() {
